e8cc784096ea49d8dae8486959a08ea8476337fb
Peter Moser <p.moser@noi.bz.it>
2020-05-29 17:33:01 +0200

    Add infrastructure and fix wcs-manifest

--- infrastructure/Jenkinsfile-CI.groovy
+++ infrastructure/Jenkinsfile-CI.groovy
@@ -1,9 +1,13 @@
 pipeline {
     agent {
         dockerfile {
-            filename 'docker/Dockerfile'
+            filename 'infrastructure/docker/Dockerfile'
             additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
         }
+    }
+
+    options {
+        ansiColor('xterm')
     }
 
     stages {
---
+++ infrastructure/Jenkinsfile-Release.groovy
@@ -0,0 +1,60 @@
+pipeline {
+    agent {
+        dockerfile {
+            filename 'infrastructure/docker/Dockerfile'
+            additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
+        }
+    }
+
+    options {
+        ansiColor('xterm')
+    }
+
+    parameters {
+        string(name: 'VERSION', defaultValue: '1.0.0', description: 'Version')
+    }
+
+    environment {
+        GIT_REPOSITORY = "git@github.com:noi-techpark/webcomp-generic-map.git"
+    }
+
+    stages {
+        stage('Clean') {
+            steps {
+                sh '''
+                  rm -rf dist node_modules
+                  yarn cache clean
+                '''
+            }
+        }
+        stage('Dependencies') {
+            steps {
+                sh 'yarn install'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'yarn run build'
+            }
+        }
+        stage('Git Tag') {
+            steps {
+                sshagent (credentials: ['jenkins_github_ssh_key']) {
+                    sh """
+                      git config --global user.email "info@opendatahub.bz.it"
+                      git config --global user.name "Jenkins"
+                      git remote set-url origin ${GIT_REPOSITORY}
+                      git add -A
+                      git commit --allow-empty -m "Version ${VERSION}"
+                      git tag --delete v${VERSION} || true
+                      git tag -a v${VERSION} -m "Version ${VERSION}"
+                      mkdir -p ~/.ssh
+                      ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+                      git push origin HEAD:master
+                      git push origin --tags
+                    """
+                }
+            }
+        }
+    }
+}
