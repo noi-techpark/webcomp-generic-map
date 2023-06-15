b623240165d12af014fff75e70e31c6fa849cf50
maxglio <massimo.maglio.06@gmail.com>
2023-06-15 13:39:35 +0200

    removed .bz.it and added .com

--- src/api/config.js
+++ src/api/config.js
@@ -3,6 +3,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 export default {
-	API_BASE_URL: 'https://mobility.api.opendatahub.bz.it/v2',
+	API_BASE_URL: 'https://mobility.api.opendatahub.com/v2',
 	ORIGIN: 'webcomp-generic-map'
 };
